f917a4998aea1efa55c53308c5c52f3eb0282904
Klijn <Klijn@users.noreply.github.com>
2014-06-22 17:06:35 +0200

    Homework assignment 2
    Signed-off-by: Klijn <Klijn@users.noreply.github.com>

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Create a "matrix" that is able to cache an inverse of itself
 
-## Write a short comment describing this function
-
+## Create a special "matrix", which is a list containing functions to get/set
+## the matrix, and to get/set the inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y){
+                x <<- y                                 ## new values for matrix
+                inv <<- NULL                            ## reset inverse matrix
+        }
+        get <- function() x                             ## return matrix
+        setsolve <- function(solve) inv <<- solve       ## calculate inverse
+        getsolve <- function() inv                      ## return inverse matrix
+        list(set=set, get=get, setsolve=setsolve, getsolve=getsolve)
 }
 
-
-## Write a short comment describing this function
-
+## Return inverse matrix of the custom "matrix" object x
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getsolve()                             ## check cache
+        if(!is.null(inv)){
+                message("retrieved cached data")
+                return(inv)                             ## return cached inverse
+        }
+        data <- x$get()                                 ## get matrix
+        inv <- solve(data, ...)                         ## calculate inverse
+        x$setsolve(inv)                                 ## cache inverse
+        inv                                             ## return inverse
 }
+
+
+## TESTING 2x2 MATRIX
+## correct inverse matrix is:
+##  0.6   -0.7
+## -0.2   0.4
+matrix2 <- makeCacheMatrix(matrix(c(4,2,7,6), nrow=2))
+cacheSolve(matrix2)
+cacheSolve(matrix2)
+
+## TESTING 3x3 MATRIX
+## correct inverse matrix is:
+##  0.2   0.2    0
+## -0.2   0.3    1
+##  0.2  -0.3    0
+matrix3 <- makeCacheMatrix(matrix(c(3,2,0,0,0,1,2,-2,1), nrow=3))
+cacheSolve(matrix3)
+cacheSolve(matrix3)
